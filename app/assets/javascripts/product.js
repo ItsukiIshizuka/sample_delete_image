09b2d1a4dfab52b5add05589d142ac049ab24ac4
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2020-01-06 20:06:50 +0900

    add delete function

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -6,9 +6,10 @@ $(function(){
     let app_img = $(`<div class="image">
                     <img id="img_prev" height="100" width="100" src="">
                     <div class="input">
+                    <div class="kara"></div>
                     <input value="" type="hidden" name="product[images_attributes][${input_length-1}][_destroy]" id="product_images_attributes_0__destroy">
-                    </div>
                     <button class="delete" type="button"></button>
+                    </div>
                     </div>`);
 
     let app_input = $(`<input class="user_img" type="file" name="product[images_attributes][${input_length}][image]">`);
@@ -33,6 +34,11 @@ $(function(){
 
 //ここから削除機能
   $(document).on("click", ".delete", function(){
+    let delete_img = $(this).parent().parent();
+    let delete_add = $(this).parent().children()[1];
+
+    $(delete_img).css('display', 'none');
+    $(delete_add).attr('value', "1");
   });
 //ここまで削除機能
 
